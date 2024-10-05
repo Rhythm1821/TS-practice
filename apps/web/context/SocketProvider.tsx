326a11a2c617739eeb91fe07f50f5f5c353bcffa
Rhythm1821 <alamrawat1821@gmail.com>
2024-10-05 18:36:48 +0530

    context added in web"

---
+++ apps/web/context/SocketProvider.tsx
@@ -0,0 +1,34 @@
+'use client'
+
+import React, { createContext, useCallback, useEffect } from "react"
+import { io } from "socket.io-client"
+
+interface SocketProviderProps {
+    children?: React.ReactNode
+}
+
+interface ISocketContext {
+    sendMessage: (message: string) => void,
+}
+
+const SocketContext = createContext<ISocketContext | null>(null)
+
+export const SocketProvider: React.FC<SocketProviderProps> = ({ children }) => {
+
+    const sendMessage: ISocketContext['sendMessage'] = useCallback((message) => {
+        console.log("Send message", message);        
+    },[])
+
+    useEffect(() => {
+        const _socket = io("http://localhost:8000")
+
+        return ()=> {
+            _socket.disconnect()
+        }
+    },[])
+    return (
+        <SocketContext.Provider value={{ sendMessage }}>
+            {children}
+        </SocketContext.Provider>
+    )
+}
