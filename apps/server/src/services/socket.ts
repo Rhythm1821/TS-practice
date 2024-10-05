8f68da2cc6e73eb14eefa8bc51a475a485e20c0d
Rhythm1821 <alamrawat1821@gmail.com>
2024-10-05 18:36:07 +0530

    initialized socket connection

--- apps/server/src/services/socket.ts
+++ apps/server/src/services/socket.ts
@@ -4,7 +4,12 @@ class SocketService {
     private _io: Server;
     constructor () {
         console.log("SocketService initialized");
-        this._io = new Server();
+        this._io = new Server({
+            cors: {
+                origin: "*",
+                allowedHeaders: ["*"]
+            }
+        });
     }
 
     public initListener() {
