e14c496ee046e2c32af07ab2b402171940571328
Rhythm1821 <alamrawat1821@gmail.com>
2024-10-06 16:35:30 +0530

    update for socket provider

--- apps/web/context/SocketProvider.tsx
+++ apps/web/context/SocketProvider.tsx
@@ -1,7 +1,7 @@
 'use client'
 
-import React, { createContext, useCallback, useEffect } from "react"
-import { io } from "socket.io-client"
+import React, { createContext, useCallback, useContext, useEffect, useState } from "react"
+import { Socket, io } from "socket.io-client"
 
 interface SocketProviderProps {
     children?: React.ReactNode
@@ -13,17 +13,32 @@ interface ISocketContext {
 
 const SocketContext = createContext<ISocketContext | null>(null)
 
+export const useSocket = () => {
+    const state = useContext(SocketContext)
+    if (!state) {
+        throw new Error("State is undefined...")
+    }
+
+    return state
+}
+
 export const SocketProvider: React.FC<SocketProviderProps> = ({ children }) => {
+    const [socket, setSocket] = useState<Socket>()
 
     const sendMessage: ISocketContext['sendMessage'] = useCallback((message) => {
         console.log("Send message", message);        
-    },[])
+        if (socket) {
+            socket.emit("event:message", { message })
+        }
+    },[socket])
 
     useEffect(() => {
         const _socket = io("http://localhost:8000")
+        setSocket(_socket)
 
         return ()=> {
             _socket.disconnect()
+            setSocket(undefined)
         }
     },[])
     return (
